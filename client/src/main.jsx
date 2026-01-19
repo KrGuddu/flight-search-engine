07e0af52ef09297527a638b0078c89a68b7d8826
KrGuddu <krguddu684@gmail.com>
2026-01-19 23:48:38 +0530

    Initial project setup done here

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
